6cd46f2205c4b85bbafdadd7e0cd14c2dcc1e746
Yong <yonghwan311@gmail.com>
2022-12-19 20:06:38 -0700

    save

--- app/src/main/java/com/example/practiceround/IntroGod.java
+++ app/src/main/java/com/example/practiceround/IntroGod.java
@@ -10,15 +10,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class
 IntroGod extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.introgod);
+        Button skip = (Button)findViewById(R.id.skip);
 
         findViewById(R.id.skip).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(IntroGod.this, Hospital1.class));
+                Intent intent = new Intent(IntroGod.this, Hospital1.class);
+                intent.putExtra("msg", skip.toString());
+                startActivity(intent);
+
+
             }
         });
     }
